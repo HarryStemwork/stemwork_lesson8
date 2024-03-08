7fc673799bc899cd5cf7b804cc99ef7363b5985c
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-03-09 00:02:26 +0800

    Create BasicStatistics.py

---
+++ BasicStatistics.py
@@ -0,0 +1,16 @@
+#values are 1100 652 946 821 955 1024 1155
+val = # define values using tuple
+mean = 
+print(f"Mean: {mean}")
+
+var = 0
+for v in val:
+    var = 
+var = 
+print(f"Variance: {var}")
+
+sd = 0
+for v in val:
+    sd = 
+sd = 
+print(f"Standard Deviation: {sd}")
